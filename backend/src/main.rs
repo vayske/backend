503e6b7d4b6ce35a1c9df1c2be043602fde7501e
SinceL <a530478271@gmail.com>
2023-09-23 04:36:49 -0700

    update

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,13 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpRequest, HttpServer, Responder};
-use actix_multipart::Multipart;
+use std::fs::File;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_multipart::{Multipart, form};
 use futures_util::TryStreamExt;
+use mime::{ Mime, IMAGE_PNG, IMAGE_JPEG, IMAGE_GIF, IMAGE_BMP };
+use tokio::{
+    fs,
+    io::AsyncWriteExt
+};
 
 #[get("/get")]
 async fn hello() -> impl Responder {
@@ -10,10 +17,25 @@ async fn hello() -> impl Responder {
 #[post("/upload")]
 async fn save_files(mut payload: Multipart) -> impl Responder {
     println!("called!");
+    let legal_filetypes: [Mime; 4] = [IMAGE_BMP, IMAGE_GIF, IMAGE_JPEG, IMAGE_PNG];
+    let dir: &str = "./images/";
     loop {
-        if let Ok(Some(field)) = payload.try_next().await {
-            println!("filename {}", field.content_disposition().get_filename().unwrap());
-        } else { break; }
+        if let Ok(Some(mut field)) = payload.try_next().await {
+            let filetype: Option<&Mime> = field.content_type();
+            if filetype.is_none() {
+                continue;
+            }
+            if !legal_filetypes.contains(&filetype.unwrap()) {
+                continue;
+            }
+            let destination: String = format!("{}{}", dir, field.content_disposition().get_filename().unwrap());
+            let mut save_image: fs::File = fs::File::create(&destination).await.unwrap();
+            while let Ok(Some(chunk)) = field.try_next().await {
+                save_image.write_all(&chunk).await.unwrap();
+            }
+        } else {
+            break;
+        }
     }
     HttpResponse::Ok().body("Hey there!")
 }
