8974d1146788a1ecce10d7fd3aba55ca59bc02d9
SinceL <a530478271@gmail.com>
2023-09-23 05:13:38 -0700

    test

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,10 +2,10 @@ use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use actix_multipart::Multipart;
 use futures_util::TryStreamExt;
 use mime::{ Mime, IMAGE_PNG, IMAGE_JPEG, IMAGE_GIF, IMAGE_BMP };
-use tokio::{
-    fs,
-    io::AsyncWriteExt
-};
+// use tokio::{
+//     fs,
+//     io::AsyncWriteExt
+// };
 
 #[get("/get")]
 async fn hello() -> impl Responder {
@@ -16,9 +16,11 @@ async fn hello() -> impl Responder {
 async fn save_files(mut payload: Multipart) -> impl Responder {
     println!("called!");
     let legal_filetypes: [Mime; 4] = [IMAGE_BMP, IMAGE_GIF, IMAGE_JPEG, IMAGE_PNG];
-    let dir: &str = "./images/";
+    //let dir: &str = "./images/";
+    //let mut image_list: HashMap<String, String> = HashMap::new();
+    let mut response_body: String = "".to_owned();
     loop {
-        if let Ok(Some(mut field)) = payload.try_next().await {
+        if let Ok(Some(field)) = payload.try_next().await {
             let filetype: Option<&Mime> = field.content_type();
             if filetype.is_none() {
                 continue;
@@ -26,16 +28,19 @@ async fn save_files(mut payload: Multipart) -> impl Responder {
             if !legal_filetypes.contains(&filetype.unwrap()) {
                 continue;
             }
-            let destination: String = format!("{}{}", dir, field.content_disposition().get_filename().unwrap());
-            let mut save_image: fs::File = fs::File::create(&destination).await.unwrap();
-            while let Ok(Some(chunk)) = field.try_next().await {
-                save_image.write_all(&chunk).await.unwrap();
-            }
+            let file_name: &str = field.content_disposition().get_filename().unwrap();
+            let tags: &str = field.content_disposition().get_name().unwrap();
+            response_body.push_str(&format!("file: {}\ttag: {}\n",file_name,tags));
+            //let destination: String = format!("{}{}", dir, file_name);
+            //let mut save_image: fs::File = fs::File::create(&destination).await.unwrap();
+            // while let Ok(Some(chunk)) = field.try_next().await {
+            //     save_image.write_all(&chunk).await.unwrap();
+            // }
         } else {
             break;
         }
     }
-    HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body(response_body)
 }
 
 #[actix_web::main]
